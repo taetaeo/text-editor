b99197fc36c4d6939e441cc1a5ca53209056eccd
taeo <ohtk1119@gmail.com>
2024-07-10 10:42:56 +0900

    feat: Add toolbar label config

--- package/src/configs/editor.config.ts
+++ package/src/configs/editor.config.ts
@@ -1,4 +1,28 @@
-const styleMap = {
+const textStyle = {
+  bold: "굵기",
+  italic: "기울림",
+  underline: "밑줄",
+};
+
+const listStyle = {
+  bullet: "bullet",
+  number: "number",
+};
+
+const colorStyle = {
+  red: "red",
+  blue: "blue",
+  green: "green",
+};
+
+const alignStyle = {
+  left: "왼쪽정렬",
+  right: "오른쪽정렬",
+  center: "가운데 정렬",
+  justify: "양쪽 정렬",
+};
+
+const customStyleMap = {
   RED: { color: "red" },
   BLUE: { color: "blue" },
   GREEN: { color: "green" },
@@ -7,8 +31,23 @@ const styleMap = {
   SIZE_20: { fontSize: "20px" },
 };
 
-const textEditorConfig = {
-  styleMap,
+const toolbarLabel = {
+  text: textStyle,
+  color: colorStyle,
+  align: alignStyle,
+  list: listStyle,
 };
+
+const textEditorConfig = {
+  customStyleMap,
+  toolbarLabel,
+};
+
+export type TextStyle = typeof textStyle;
+export type ListStyle = typeof listStyle;
+export type ColorStyle = typeof colorStyle;
+export type AlignStyle = typeof alignStyle;
+export type CustomStyleMap = typeof customStyleMap;
+export type TextEditorConfig = typeof textEditorConfig;
 
 export default textEditorConfig;
--- package/src/configs/index.ts
+++ package/src/configs/index.ts
@@ -1,1 +1,3 @@
 export { default as textEditorConfig } from "./editor.config";
+
+export type { TextEditorConfig, CustomStyleMap } from "./editor.config";
